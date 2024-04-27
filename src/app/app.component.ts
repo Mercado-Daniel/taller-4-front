7192729ca1eb03daa81b614bb674a80e4bff95ab
Mercado-Daniel <daniel-jtag@hotmail.com>
2024-04-26 21:33:03 -0300

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MiservicioService } from './miservicio.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'animehub';
+  title = 'Angular';
+  titulo = 'Hola';
+
+  getTitle(){
+    return this.title;
+  }
+
+  constructor(private service:MiservicioService){
+
+  }
+
+  getEmpleado(){
+    return this.service.getEmpleados();
+  }
 }
